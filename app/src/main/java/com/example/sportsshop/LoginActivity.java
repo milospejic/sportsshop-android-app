11930bbae7d89d8fa89626c3fadc7f5aa6da093d
Milos <milospejic2001@gmail.com>
2023-04-09 15:30:30 +0200

    Connected to Firebase, enabled sign in and sign up functionalities

--- app/src/main/java/com/example/sportsshop/LoginActivity.java
+++ app/src/main/java/com/example/sportsshop/LoginActivity.java
@@ -1,21 +1,70 @@
 package com.example.sportsshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText email,password;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
     }
 
     public void signin(View view) {
-        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Enter email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(userPassword.length()<8){
+            Toast.makeText(this, "Password must contain at least 8 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if(task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this,"Successfully signed in!", Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                }else{
+                                    Toast.makeText(LoginActivity.this,"Unsuccessful sign in!" + task.getException(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
     }
 
     public void signup(View view) {
--- app/src/main/java/com/example/sportsshop/RegistrationActivity.java
+++ app/src/main/java/com/example/sportsshop/RegistrationActivity.java
@@ -1,21 +1,92 @@
 package com.example.sportsshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    EditText name, email,  password, confirm_password;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        getSupportActionBar().hide();
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+            finish();
+        }
+
+        name = findViewById(R.id.name);
+        email= findViewById(R.id.email);
+        password= findViewById(R.id.password);
+        confirm_password = findViewById(R.id.confirm_password);
+
+
     }
 
     public void signup(View view) {
-        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this, "Enter name!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Enter email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(userPassword.length()<8){
+            Toast.makeText(this, "Password must contain at least 8 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!userPassword.equals(confirm_password.getText().toString())){
+            Toast.makeText(this, "Passwords not matching!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegistrationActivity.this,"Successfully signed up!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                        }else{
+                            Toast.makeText(RegistrationActivity.this, "Unsuccessful sign up" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 
     public void signin(View view) {
