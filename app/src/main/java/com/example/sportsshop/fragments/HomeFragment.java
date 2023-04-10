86cc9af36df7f9646215bcf76e1ed21bfbc66c6a
Milos <milospejic2001@gmail.com>
2023-04-10 21:39:11 +0200

    Created category list functionality as part of the home fragment

--- app/src/main/java/com/example/sportsshop/fragments/HomeFragment.java
+++ app/src/main/java/com/example/sportsshop/fragments/HomeFragment.java
@@ -2,7 +2,10 @@ package com.example.sportsshop.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,13 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.sportsshop.R;
+import com.example.sportsshop.adapters.CategoryAdapter;
+import com.example.sportsshop.models.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +30,14 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
 
+    RecyclerView catRecyclerView;
+
+
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+
+
+    FirebaseFirestore db;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -32,6 +50,11 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        catRecyclerView= root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
+
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
 
@@ -40,6 +63,31 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.air, "Air Force 1", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+
+
+        catRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL, false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(),categoryModelList);
+        catRecyclerView.setAdapter(categoryAdapter);
+
+
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document : task.getResult()) {
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+                            }
+                        }else{
+
+                        }
+                    }
+                });
         return root;
     }
 }
