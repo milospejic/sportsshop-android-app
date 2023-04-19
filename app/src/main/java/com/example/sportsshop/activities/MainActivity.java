36dbbe5ebb772ac0bccf6865abb8d3089822dcb4
Milos <milospejic2001@gmail.com>
2023-04-19 20:55:39 +0200

    Created home toolbar and enabled log out functionality, also created CartActivity(no implementation yet)

--- app/src/main/java/com/example/sportsshop/activities/MainActivity.java
+++ app/src/main/java/com/example/sportsshop/activities/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.sportsshop.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import com.example.sportsshop.R;
 import com.example.sportsshop.fragments.HomeFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Fragment homeFragment;
+    FirebaseAuth auth;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu);
+        }
         homeFragment = new HomeFragment();
         loadFragment(homeFragment);
     }
@@ -25,5 +41,29 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.home_container,homeFragment);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_logout){
+
+            auth.signOut();
+            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            finish();
+        }else if (id == R.id.menu_cart){
+            startActivity(new Intent(MainActivity.this,CartActivity.class));
+
+        }
+        return true;
     }
 }
